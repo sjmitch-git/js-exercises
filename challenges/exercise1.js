b2639608a033ae092e8d1a65c3ebc594ebac53e4
sjmitch-git <sjmitch20@outlook.com>
2023-10-09 17:19:19 +0100

    fix: exercise1 tests

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,24 +1,32 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+  if (people === undefined) throw new Error("people is required");
+  // A bus can hold 40 people. This function should return how many buses are required for the number of people
+  return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+  if (arr === undefined) throw new Error("arr is required");
+  let count = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] == "sheep") count++;
+  }
+  return count;
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  return (
+    person.address.postCode.charAt(0) === "M" &&
+    person.address.city === "Manchester"
+  );
 }
